ae75c393d8479b5aa8ff24432aae3bb9339a9deb
ramanujdas <ramanujds9@gmail.com>
2020-10-10 17:07:43 +0530

    Polimorphism Demos

---
+++ polymorphsm-examples/src/com/example/MethodOverloading.java
@@ -0,0 +1,19 @@
+package com.example;
+
+
+
+public class MethodOverloading {
+	
+	public int add(int a,int b) {
+		return a+b;
+	}
+	
+	public float add(float a, float b) {
+		return a+b;
+	}
+	
+	public int add(int a, int b, int c) {
+		return a+b+c;
+	}
+
+}
---
+++ polymorphsm-examples/src/com/mainmethod/App.java
@@ -0,0 +1,13 @@
+package com.mainmethod;
+
+import com.example.MethodOverloading;
+
+public class App {
+
+	public static void main(String[] args) {
+		MethodOverloading mt=new MethodOverloading();
+		System.out.println(mt.add(5,(int)Math.ceil(2.01)));
+
+	}
+
+}
