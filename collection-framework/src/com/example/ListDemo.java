11f4dc6192639a8f5bf85707ef38b97e69632ac7
ramanujdas <ramanujds9@gmail.com>
2020-11-04 19:47:02 +0530

    List and Collection

--- collection-framework/src/com/example/ListDemo.java
+++ collection-framework/src/com/example/ListDemo.java
@@ -3,6 +3,7 @@ package com.example;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 public class ListDemo {
 
@@ -10,24 +11,36 @@ public class ListDemo {
 	public static void main(String[] args) {
 		
 		
-		Collection items= new ArrayList();
-		
+		List items=new ArrayList();
 		items.add("Kush");
 		items.add(5);
 		items.add(true);
-		items.add(25.65);
+		items.add(2.5);
 		
-		for(Object obj:items) {
-			System.out.println(obj);
-		}
+		items.add(2, "Mohit");
 		
-//		items.forEach(obj->System.out.println(obj));
+//		for(Object obj:items) {
+//			System.out.println(obj);
+//		}
 		
 //		Iterator itr=items.iterator();
 //		while(itr.hasNext()) {
 //			System.out.println(itr.next());
 //		}
 		
+//		for(int i=0;i<items.size();i++) {
+//			System.out.println(items.get(i));
+//		}
+		
+		items.forEach(System.out::println);
+		
+		
+		
+//		int []arr= {5,2,1,6,3};
+//		
+//		for(int value:arr) {
+//			System.out.println(value);
+//		}
 		
 		
 
